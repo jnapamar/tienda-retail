262573548d17d544d1cafb5eb280112c048ea941
jnapamar <jose.napa.m@gmail.com>
2026-07-21 09:29:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 
 // 1. MIDDLEWARES
 app.use(cors());
-app.use(express.json());
+// Aumentamos el límite de tamaño para recibir imágenes Base64 grandes desde el PC
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 5000;
